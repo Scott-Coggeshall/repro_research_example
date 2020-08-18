0b8c6f1e5c8ef18d4e6b386313b219a9943fb996
Scott Coggeshall <sscogges@uw.edu>
2020-08-18 16:35:42 -0700

    add other random effects models to plan

--- r/plan.R
+++ r/plan.R
@@ -5,7 +5,13 @@ plan <- drake_plan(
   
   cleaned_data = clean_data(uncleaned_data),
   
-  stan_model = rstanarm::stan_glmer(count ~ swab_type + (1 | id) + (1 | plate_number), family = 'poisson', data = cleaned_data),
+  stan_model = rstanarm::stan_glmer(count ~ swab_type + (1 | id) + (1 | plate_number), family = 'poisson', data = cleaned_data %>% filter(censored == FALSE)),
+  
+  stan_model_noplate = rstanarm::stan_glmer(count ~ swab_type + (1 | id), family = "poisson", data = cleaned_data %>% filter(censored == FALSE)),
+  
+  stan_model_noid = rstanarm::stan_glmer(count ~ swab_type + (1 | plate_number), family = "poisson", data = cleaned_data %>% filter(censored == FALSE)),
+  
+  stan_model_noranef = rstanarm::stan_glm(count ~ swab_type, family = "poisson", data = cleaned_data %>% filter(censored == FALSE)),
   
   model_samples = rstan::extract(stan_model$stanfit),
   
